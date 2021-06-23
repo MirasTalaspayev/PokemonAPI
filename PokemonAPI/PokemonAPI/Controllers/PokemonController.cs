9b8df7f45d452e547ec32f0b9a172ef59b64c854
Miras Talaspayev <tmk01.12.00@gmail.com>
2021-06-23 13:53:43 +0600

    Added: Filters the query, which returns pokemons that are satisfied by the filter conditions.

--- PokemonAPI/PokemonAPI/Controllers/PokemonController.cs
+++ PokemonAPI/PokemonAPI/Controllers/PokemonController.cs
@@ -20,26 +20,52 @@ namespace PokemonAPI.Controllers
             csvFile = _csvfile;
         }
         [HttpGet]
-        public IEnumerable<Pokemon> GetAll(int page = 1, string name = null) //, string[] filters = null
+        public IEnumerable<Pokemon> GetAll(int page = 1, string name = null)
         {
             List<Pokemon> pokemons = csvFile.GetPokemons();
+            var query = HttpContext.Request.Query;
+            int max, min;
+            
+            #region // filters the query
+            if (query.ContainsKey("hp[lte]") && int.TryParse(query["hp[lte]"], out max))
+            {
+                pokemons = pokemons.Where(p => p.HP <= max).ToList();
+            }
+            if (query.ContainsKey("hp[gte]") && int.TryParse(query["hp[gte]"], out min))
+            {
+                pokemons = pokemons.Where(p => p.HP >= min).ToList();
+            }
+            if (query.ContainsKey("attack[lte]") && int.TryParse(query["attack[lte]"], out max))
+            {
+                pokemons = pokemons.Where(p => p.Attack <= max).ToList();
+            }
+            if (query.ContainsKey("attack[gte]") && int.TryParse(query["attack[gte]"], out min))
+            {
+                pokemons = pokemons.Where(p => p.Attack >= min).ToList();
+            }
+            if (query.ContainsKey("defense[lte]") && int.TryParse(query["defense[lte]"], out max))
+            {
+                pokemons = pokemons.Where(p => p.Defense <= max).ToList();
+            }
+            if (query.ContainsKey("defense[gte]") && int.TryParse(query["defense[gte]"], out min))
+            {
+                pokemons = pokemons.Where(p => p.Defense >= min).ToList();
+            }
+            #endregion
+            /* Searches Pokemon by 'Name' parameter
+             * Firstly, by exact match;
+             * Secondly, by Names that Contains 'name' in the string
+             */
             if (!string.IsNullOrEmpty(name))
             {
                 var poks = pokemons.Where(p => p.Name == name);
                 pokemons = pokemons.Where(p => p.Name.Contains(name) && p.Name != name).ToList();
                 pokemons.InsertRange(0, poks);
             }
-            //if (filters != null)
-            //{
-            //    foreach (string filter in filters)
-            //    {
-            //        Console.WriteLine(filter);
-            //    }
-            //}
-            
+
             var model = PagingList.Create(pokemons, 10, page);
             return model;
         }
-        
+
     }
 }
